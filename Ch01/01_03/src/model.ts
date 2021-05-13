96dfa1921ad6203115c94af1ba7aedd970588f23
Angela Mitchell <anglarenmit@gmail.com>
2021-05-12 21:51:23 -0500

    Learn about type inference and gradual typing

--- Ch01/01_03/src/model.ts
+++ Ch01/01_03/src/model.ts
@@ -1,22 +1,27 @@
 let displayName: string = "Jess's standing desk";
-let inventoryType: string = "furniture";
-let trackingNumber: string = "FD123455";
+let inventoryType: string = 'furniture';
+let trackingNumber: string = 'FD123455';
 let createDate: Date = new Date();
 let originalCost: number = 425;
+// let originalCost = 425 as number;
+
+// Use any type only if you want to opt out of type safety
+// let originalCost: any = 425;
+// OR
+// let originalCost = 425 as any;
+// originalCost = "A LOT of money!";
 
 function getInventoryItem(trackingNumber: string): {
-    displayName: string;
-    inventoryType: string;    
-    trackingNumber: string;    
-    createDate: Date;    
-    originalCost: number;    
+  displayName: string;
+  inventoryType: string;
+  trackingNumber: string;
+  createDate: Date;
+  originalCost: number;
 } {
-    return null;
+  return null;
 }
 
-function saveInventoryItem(item) {
-
-}
+function saveInventoryItem(item) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
