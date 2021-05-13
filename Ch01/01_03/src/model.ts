bb64821258b221d44b8f8471a23a7ec01cc88d93
Angela Mitchell <anglarenmit@gmail.com>
2021-05-12 22:00:26 -0500

    Add more notes about type inference and gradual typing

--- Ch01/01_03/src/model.ts
+++ Ch01/01_03/src/model.ts
@@ -3,9 +3,18 @@ let inventoryType: string = 'furniture';
 let trackingNumber: string = 'FD123455';
 let createDate: Date = new Date();
 let originalCost: number = 425;
+
+// Type inference => TypeScript is able to figure out the type of many variables w/o you having to provide any info at all
+
+// Type inference allows TypeScript to find errors in your code at compile time by automatically discovering type info
+
 // let originalCost = 425 as number;
+// originalCost = "A LOT of money!";
+
+// Gradual typing => allows you to choose when and how TypeScript applies type to your code
 
 // Use any type only if you want to opt out of type safety
+
 // let originalCost: any = 425;
 // OR
 // let originalCost = 425 as any;
