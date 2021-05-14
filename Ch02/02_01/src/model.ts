8c9b3a5047097103a0bebbdb5fde33ac325e6da7
Angela Mitchell <anglarenmit@gmail.com>
2021-05-13 23:16:28 -0500

    Define a custome type with an interface

--- Ch02/02_01/src/model.ts
+++ Ch02/02_01/src/model.ts
@@ -1,22 +1,28 @@
 let displayName: string = "Jess's standing desk";
-let inventoryType: string = "furniture";
-let trackingNumber: string = "FD123455";
+let inventoryType: string = 'furniture';
+let trackingNumber: string = 'FD123455';
 let createDate: Date = new Date();
 let originalCost = 425;
 
-function getInventoryItem(trackingNumber: string): {
-    displayName: string;
-    inventoryType: string;
-    trackingNumber: string;
-    createDate: Date;
-    originalCost: number;
-} {
-    return null;
+// Interface definitions have no impact on the code that's rendered
+
+// First, the TypeScript compiler digests the interface to remember them for later; then, it completely removes them from the code
+
+// Interfaces only exist to give TypeScript more info about the code you're writing and help you find mistakes
+
+interface InventoryItem {
+  displayName: string;
+  inventoryType: string;
+  trackingNumber: string;
+  createDate: Date;
+  originalCost: number;
 }
 
-function saveInventoryItem(item) {
-
+function getInventoryItem(trackingNumber: string): InventoryItem {
+  return null;
 }
+
+function saveInventoryItem(item: InventoryItem) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
