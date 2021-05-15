4bfc06b0dbd7981f2429f5acc2c78652c932a65d
Angela Mitchell <anglarenmit@gmail.com>
2021-05-14 22:02:53 -0500

    Add type support for third-party APIs

--- Ch04/04_04/src/model.ts
+++ Ch04/04_04/src/model.ts
@@ -1,39 +1,38 @@
 let displayName: string = "Jess's standing desk";
-let inventoryType: string = "furniture";
-let trackingNumber: string = "FD123455";
+let inventoryType: string = 'furniture';
+let trackingNumber: string = 'FD123455';
 let createDate: Date = new Date();
 
 type Cost = number | string;
 
 let originalCost: Cost;
 
-if (typeof originalCost === "number") {
-    let cost: number = originalCost;
+if (typeof originalCost === 'number') {
+  let cost: number = originalCost;
 } else {
-    let x = originalCost;
+  let x = originalCost;
 }
 
 enum InventoryItemType {
-    Computer = "computer",
-    Furniture = "furniture"
+  Computer = 'computer',
+  Furniture = 'furniture',
 }
 
 interface InventoryItem {
-    displayName: string;
-    inventoryType: "computer" | "furniture";
-    readonly trackingNumber: string;
-    createDate: Date;
-    originalCost?: number;
+  displayName: string;
+  inventoryType: 'computer' | 'furniture';
+  readonly trackingNumber: string;
+  createDate: Date;
+  originalCost?: number;
 
-    addNote?: (note: string) => string;
+  addNote?: (note: string) => string;
 }
 
 function getInventoryItem(trackingNumber: string): InventoryItem {
-    return null;
+  return null;
 }
 
-function saveInventoryItem(item: InventoryItem) {
-}
+function saveInventoryItem(item: InventoryItem) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
@@ -42,23 +41,25 @@ let updatedInventoryItem = inventoryItem;
 inventoryItem.createDate = new Date();
 
 saveInventoryItem({
-    displayName: "MacBook Pro 15 Retina",
-    inventoryType: "computer",
-    trackingNumber: "MBP123456",
-    createDate: new Date(),
+  displayName: 'MacBook Pro 15 Retina',
+  inventoryType: 'computer',
+  trackingNumber: 'MBP123456',
+  createDate: new Date(),
 });
 
 function clone<T, U>(source: T, options: U): T {
-    const serialized = JSON.stringify(source);
-    return JSON.parse(serialized);
+  const serialized = JSON.stringify(source);
+  return JSON.parse(serialized);
 }
 
 const cloned = clone(inventoryItem, { deep: true });
 
 class KeyValuePair<TKey, TValue> {
-    Key: TKey;
-    Value: TValue;
+  Key: TKey;
+  Value: TValue;
 }
 
-var keyValue: KeyValuePair<string, number> = { Key: "something", Value: 1234 };
+var keyValue: KeyValuePair<string, number> = { Key: 'something', Value: 1234 };
 var keyValue2: KeyValuePair<number, boolean> = { Key: 1234, Value: true };
+
+declare var Vue: any;
